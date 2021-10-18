02f0dbacc7a983e2a9c96dae9b467ab01501eb4b
Ivanho92 <ivanho92@hotmail.be>
2021-10-18 10:41:05 +0200

    HTML implementation of the front-page

---
+++ wp-content/themes/interior-alchemy/components/navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+    <div class="container">
+        <a class="text-uppercase navbar-brand brand-logo" href="index.php">Interior Alchemy</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="text-reset nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Offerings</a>
+                    <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarScrollingDropdown">
+                        <li><a class="text-reset dropdown-item" href="offering.php">Interior Analysis & Design</a></li>
+                        <li><a class="text-reset dropdown-item" href="offering.php">Yoga</a></li>
+                        <li><a class="text-reset dropdown-item" href="offering.php">Reiki & Channeling</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="text-reset nav-link" href="about.php">About</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- wp-content/themes/interior-alchemy/front-page.php
+++ wp-content/themes/interior-alchemy/front-page.php
@@ -14,24 +14,45 @@
         )
     );
     $query = new WP_Query($args);
-    // var_dump($query); die;
     ?>
 
-<div class="container">
-    <h1>Front-Page</h1>
-    <h2>Offres</h2>
-    
-    <?php if ($query->have_posts()): while ($query->have_posts()): $query->the_post(); ?>
-    <article>
-        <a href="<?php the_permalink(); ?>">
-            <strong class="post-title"><?php the_title(); ?></strong>
-        </a>
-    </article>
-    <?php endwhile; else: ?>
-        <p>Aucune offre pour le moment...</p>
-    <?php endif; ?>
+<small>Front-Page</small>
 
-</div>
+<?php include(get_template_directory() . "/components/navbar.php"); ?>
 
+    <div class="jumbotron mt-5 p-5 mb-3 rounded-3">
+        <div class="text-center container-fluid py-5">
+            <h1 class="mt-5 mb-4 website-title text-uppercase display-2 fw-bold text-light">Interior Alchemy</h1>
+            <p class="website-baseline display-6 text-light">a return to home</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <h2 class="display-4 py-3 text-center">Offerings</h2>
+        <hr>
+        <p class="text-center mb-5">A methodology developped by Mathilde Grimm that utilizes a range of holistic healing modalities to facilititate a return to home - within and without.</p>
+        
+        <?php if ($query->have_posts()) : ?>
+            <?php while ($query->have_posts()): $query->the_post(); ?>
+            <div class="row justify-content-around text-center">
+                <div class="mb-5 col-md-3 d-flex flex-column justify-content-start">
+                    <div>
+                        <img class="mx-auto mb-3 offering-miniature" src="img/offering.jfif" alt="Offering #1">
+                    </div>
+                    <div>
+                        <h3><?php the_title(); ?></h3>
+                    </div>
+                    <div>
+                        <p>Some representative placeholder content for the three columns of text below the carousel. This is
+                            the
+                            first column.</p>
+                        <p><a class="btn btn-cta" href="<?php the_permalink(); ?>">More details</a></p>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile; ?>
+        <?php else: ?>
+            <p class="text-center">Aucune offre pour le moment...</p>
+        <?php endif; ?>
 
 <?php get_footer() ?>
